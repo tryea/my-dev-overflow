562524d2e09ec9d388c3157056c8d1540621a7ad
Ersapta Aristo <aristoersapta@gmail.com>
2024-01-16 22:28:06 +0700

    [016-create-navbar] add layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type RootLayoutProps = {
+    children: React.ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+    return (
+        <main className="background-light850_dark100 relative">
+            Navbar
+            <div className="flex">
+                Leftsidebar
+                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+                    <div className="mx-auto w-full max-w-5xl">{children}</div>
+                </section>
+                Rightsidebar
+            </div>
+            Toaster
+        </main>
+    );
+};
+
+export default RootLayout;
